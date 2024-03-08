1f8d6c51415e4c946ddc544e0f25ad12ca584979
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-03-08 14:29:39 +0100

    Clean Coding in the script.js and added jsdoc comments

--- script/script.js
+++ script/script.js
@@ -1,26 +1,39 @@
 // Arrays
 let notes = [];
 let deletedNotes = [];
+
+
+/**
+ * This function loads the data in the beginning.
+ */
 load();
 
 
-function render() {
-    document.getElementById('deletedNotesActiv').classList.remove('btn-color');
-    document.getElementById('notesActiv').classList.add('btn-color');
+/**
+ * This function loads the saved notes and deleted notes as Strings from the local Storage and converts it to a Arrays.
+ */
+function load() {
+    let notesAsText = localStorage.getItem('notes');
+    let notesAsDeletedText = localStorage.getItem('deletedNotes');
+    if (notesAsText) {
+        notes = JSON.parse(notesAsText);
+    }
+    if (notesAsDeletedText) {
+        deletedNotes = JSON.parse(notesAsDeletedText);
+    }
+}
 
+
+/**
+ * This function renders the input box, as well as the notes or the text, when there are no notes.
+ */
+function render() {
+    setButtonNotes();
     noteInputBox();
     let contentShowNotes = document.getElementById('contentShowNotes');
     contentShowNotes.innerHTML = '';
-
     if (notes.length > 0) {
-        contentShowNotes.innerHTML = /*html*/`
-            <div class="flex-wrap-container" id="flexWrapContainer"></div>
-        `;
-        for (let i = 0; i < notes.length; i++) {
-            const flexWrapContainer = document.getElementById('flexWrapContainer');
-            const note = notes[i];
-            flexWrapContainer.innerHTML += addContentNotes(note, i);
-        }
+        renderNotes(contentShowNotes);
     }
     else {
         contentShowNotes.innerHTML = showNoNotes();
@@ -28,6 +41,18 @@ function render() {
 }
 
 
+/**
+ * The notes button gets the background-color.
+ */
+function setButtonNotes() {
+    document.getElementById('deletedNotesActiv').classList.remove('btn-color');
+    document.getElementById('notesActiv').classList.add('btn-color');
+}
+
+
+/**
+ * This functions shows the input box.
+ */
 function noteInputBox() {
     let content = document.getElementById('content');
     content.innerHTML = /*html*/ `
@@ -45,8 +70,30 @@ function noteInputBox() {
 }
 
 
+/**
+ * This function renders the notes.
+ * @param {HTMLElement} contentShowNotes - The HTML element, in which the flex-wrap-container with the notes comes.
+ */
+function renderNotes(contentShowNotes) {
+    contentShowNotes.innerHTML = /*html*/`
+        <div class="flex-wrap-container" id="flexWrapContainer"></div>
+    `;
+    for (let i = 0; i < notes.length; i++) {
+        const flexWrapContainer = document.getElementById('flexWrapContainer');
+        const note = notes[i];
+        flexWrapContainer.innerHTML += addContentNotes(note, i);
+    }
+}
+
+
+/**
+ * This function makes a note.
+ * @param {String} note - This is the note from the Array with the current index.
+ * @param {Number} i - This is the index of the note.
+ * @returns - The HTML element of the note.
+ */
 function addContentNotes(note, i) {
-   return /*html*/ `
+    return /*html*/ `
     <div class="note-section withInput">
         <p class="note">${note}</p>
         <div class="container-trash">
@@ -57,14 +104,22 @@ function addContentNotes(note, i) {
 }
 
 
+
+/**
+ * This function gets selected, when there are no notes.
+ * @returns - The HTML element, when there are no notes.
+ */
 function showNoNotes() {
     return /*html*/`
     <img class="main-icon rose" src="./img/logo-icon.svg">
     <p class="p-without" >Hier werden hinzugefügte Notizen angezeigt</p>
     `;
-} 
+}
 
 
+/**
+ * This functions adds the note to the array and shows it under the input field.
+ */
 function addNote() {
     let noteInput = document.getElementById('noteInput');
     let updatedNote = noteInput.value.replace(/\n/g, '<br>');
@@ -72,7 +127,6 @@ function addNote() {
         alert('Bitte gebe eine Notiz ein.');
     } else {
         notes.push(updatedNote);
-
         save();
         render();
         noteInput.value = '';
@@ -80,7 +134,9 @@ function addNote() {
 }
 
 
-
+/**
+ * This function saves the notes in the localStorage.
+ */
 function save() {
     let notesAsText = JSON.stringify(notes);
     let deletedNotesAsText = JSON.stringify(deletedNotes);
@@ -90,18 +146,10 @@ function save() {
 }
 
 
-function load() {
-    let notesAsText = localStorage.getItem('notes');
-    let notesAsDeletedText = localStorage.getItem('deletedNotes');
-    if(notesAsText) {
-        notes = JSON.parse(notesAsText);
-    }
-    if(notesAsDeletedText) {
-        deletedNotes = JSON.parse(notesAsDeletedText);
-    }
-}
-
-
+/**
+ * This function shifts the notes from the array notes to the array deleted notes.
+ * @param {Number} noteElement - The index of the element, which gets shifted.
+ */
 function shiftNotesToTrash(noteElement) {
     const removedNote = notes[noteElement];
     deletedNotes.push(removedNote);
@@ -111,6 +159,10 @@ function shiftNotesToTrash(noteElement) {
 }
 
 
+/**
+ * This function deletes the notes from the trash.
+ * @param {Number} i - The index of the element, which gets deleted.
+ */
 function deleteNote(i) {
     deletedNotes.splice(i, 1);
     showDeletedNotes();
@@ -118,39 +170,70 @@ function deleteNote(i) {
 }
 
 
+/**
+ * This function refreshes the side.
+ */
 function refresh() {
     render();
     save();
 }
 
 
+/**
+ * This function renders the notes, when you click on the notepad-icon.
+ */
 function showNotes() {
     render();
 }
 
 
+/**
+ * This function renders the deleted notes or the text, when there are no deleted notes.
+ */
 function showDeletedNotes() {
-    document.getElementById('deletedNotesActiv').classList.add('btn-color');
-    document.getElementById('notesActiv').classList.remove('btn-color');
-
+    setButtonDeletedNotes();
     let deletedContent = document.getElementById('content');
     deletedContent.innerHTML = '';
-
     if (deletedNotes.length > 0) {
-        deletedContent.innerHTML = /*html*/`
-            <div class="flex-wrap-container" id="flexWrapContainer"></div>
-        `;
-        for (let i = 0; i < deletedNotes.length; i++) {
-        const flexWrapContainer = document.getElementById('flexWrapContainer');
-        const deletedNote = deletedNotes[i];
-        flexWrapContainer.innerHTML += addDeletedContentNotes(deletedNote, i);
-        }
+        renderDeletedNotes(deletedContent);
     }
     else {
         content.innerHTML = showNoDeletedNotes();
     }
 }
 
+
+/**
+ * The trash button gets the background-color.
+ */
+function setButtonDeletedNotes() {
+    document.getElementById('deletedNotesActiv').classList.add('btn-color');
+    document.getElementById('notesActiv').classList.remove('btn-color');
+}
+
+
+/**
+ * This function renders the deleted notes.
+ * @param {HTMLElement} deletedContent - The HTML element, in which the flex-wrap-container with the notes comes.
+ */
+function renderDeletedNotes(deletedContent) {
+    deletedContent.innerHTML = /*html*/`
+    <div class="flex-wrap-container" id="flexWrapContainer"></div>
+    `;
+    for (let i = 0; i < deletedNotes.length; i++) {
+        const flexWrapContainer = document.getElementById('flexWrapContainer');
+        const deletedNote = deletedNotes[i];
+        flexWrapContainer.innerHTML += addDeletedContentNotes(deletedNote, i);
+    }
+}
+
+
+/**
+ * This function shows the deleted notes.
+ * @param {String} deletedNote - The current deleted note.
+ * @param {Number} i - This is the index of the deleted note.
+ * @returns 
+ */
 function addDeletedContentNotes(deletedNote, i) {
     return /*html*/ `
     <div class="note-section withInput">
@@ -163,6 +246,11 @@ function addDeletedContentNotes(deletedNote, i) {
     `;
 }
 
+
+/**
+ * This function gets showed, when there are no deleted notes.
+ * @returns - The html element gets returned.
+ */
 function showNoDeletedNotes() {
     return /*html*/ `
     <img class="main-icon rose" src="./img/logo-icon.svg">
@@ -170,6 +258,11 @@ function showNoDeletedNotes() {
     `;
 }
 
+
+/**
+ * This function reloads a note from the trash.
+ * @param {Number} noteElement - The index of the deleted note, which gets reloaded.
+ */
 function reloadNote(noteElement) {
     const reloadNote = deletedNotes[noteElement];
     notes.push(reloadNote);
